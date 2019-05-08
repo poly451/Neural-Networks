88a918c3d15508704c1f45a127b5a0e1ee3ad21f
poly451 <49294069+poly451@users.noreply.github.com>
2019-05-07 19:21:13 -0700

    Update draw_points_v01.py

--- draw_points_v01.py
+++ draw_points_v01.py
@@ -1,3 +1,8 @@
+"""
+This code had its origin here:
+https://pythonspot.com/matplotlib-scatterplot/
+https://matplotlib.org/gallery/shapes_and_collections/scatter.html#sphx-glr-gallery-shapes-and-collections-scatter-py
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
